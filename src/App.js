b47b87798e4764679e871d1530505a1a344612b9
hokang080 <hokang080@naver.com>
2025-09-19 14:25:10 +0900

    refactor:appjs수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
 import SuggestionPage from "./pages/suggestion";
 import NoticePage from "./pages/notice";
+import SuggestionWrite from "./pages/suggestion_write";
+import SuggestionCheck from "./pages/suggestion_check";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/suggestion" element={<SuggestionPage />} />
           <Route path="/notice" element={<NoticePage />}/>
+          <Route path="/suggestion_write" element={<SuggestionWrite />} />
+          <Route path="/suggestion_check" element={<SuggestionCheck />} />
         </Routes>
       </Wrapper>
     </>
